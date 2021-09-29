909a83a7728a59d83951c97e2b5fe39341b677ae
Lufeng <chenlf@udel.edu>
2021-09-30 03:58:25 +0800

    Adding a little bit css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 import QingQing from '../src/My cat/QingQing.png';
 import q1 from '../src/My cat/q1.jpg';
 import q2 from '../src/My cat/q2.jpg';
 import q3 from '../src/My cat/q3.jpg';
 import q4 from '../src/My cat/q4.jpg';
 import React, { useState } from 'react';
-
+import { Button } from 'react-bootstrap';
+import { Container,Row,Col } from 'react-bootstrap';
 
 
 function App() { 
 
   const [index,setIndex] = useState(0);
+  
   const [pic] = useState([q1, q2, q3, q4, QingQing]);
 
   const onClickNext= () => {
@@ -24,24 +28,26 @@ function App() {
       }
     }
   
-    const onClickPrevious= () => {
-      if (index - 1 === -1 ){
-          setIndex(
-              pic.length - 1
-            )
-        } else {
-          setIndex(
-              index - 1
-            )
-        }
+  const onClickPrevious= () => {
+    if (index - 1 === -1 ){
+        setIndex(
+            pic.length - 1
+          )
+      } else {
+        setIndex(
+            index - 1
+          )
       }
+    }
       
       return (
-          <div>
-            <img src={pic[index]} style={{"maxHeight":"40%","maxWidth":"40%"}} alt="My cat" /> <br/>
-            <button style={{"fontSize":"18px"}} onClick={onClickPrevious}> Previous </button>
-            <button style={{"margin-left":"5px", "fontSize":"18px"}} onClick={onClickNext}> Next </button>
-          </div>
+          <Container className="App">
+            <Row>
+              <img src={pic[index]} style={{"maxHeight":"40%","maxWidth":"40%"}} alt="My cat" /> <br/>
+              <Col><Button style={{"fontSize":"18px"}} onClick={onClickPrevious}> Previous </Button></Col>
+              <Col><Button style={{"margin-left":"5px", "fontSize":"18px"}} onClick={onClickNext}> Next </Button></Col>
+            </Row>
+          </Container>
         );
 }
 
