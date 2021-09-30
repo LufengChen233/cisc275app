083850dba2bdc09821b67b881f2932fd584798e5
Lufeng <chenlf@udel.edu>
2021-10-01 00:44:44 +0800

    Add more pictures

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import q1 from '../src/My cat/q1.jpg';
 import q2 from '../src/My cat/q2.jpg';
 import q3 from '../src/My cat/q3.jpg';
 import q4 from '../src/My cat/q4.jpg';
+import q5 from '../src/My cat/q5.jpg';
+import q6 from '../src/My cat/q6.jpg';
+import q7 from '../src/My cat/q7.jpg';
+import q8 from '../src/My cat/q8.jpg';
+import q9 from '../src/My cat/q9.jpg';
+import q10 from '../src/My cat/q10.jpg';
+import q11 from '../src/My cat/q11.jpg';
+import q12 from '../src/My cat/q12.jpg';
+import q13 from '../src/My cat/q13.jpg';
+import q14 from '../src/My cat/q14.jpg';
+import q15 from '../src/My cat/q15.jpg';
+import q16 from '../src/My cat/q16.jpg';
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -16,7 +28,7 @@ function App() {
 
   const [index,setIndex] = useState(0);
   
-  const [pic] = useState([q1, q2, q3, q4, QingQing]);
+  const [pic] = useState([QingQing, q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14, q15, q16]);
 
   const onClickNext= () => {
     if (index + 1 === pic.length ){
@@ -52,7 +64,7 @@ function App() {
               <Card.Body>
                 <Card.Title>Gallery</Card.Title>
                 <Card.Text>
-                <div><img src={pic[index]} style={{"maxHeight":"70%","maxWidth":"70%"}} alt="Tankyou for watching " /> <br/></div>
+                <div><img src={pic[index]}  style={{"maxHeight":"70%","maxWidth":"70%"}} alt="Thank you for watching" /></div>
                 </Card.Text>
               </Card.Body>
               </Card>
--- src/components/onClickPrevious.js
+++ src/components/onClickPrevious.js
@@ -3,6 +3,18 @@ import q1 from '../My cat/q1.jpg';
 import q2 from '../My cat/q2.jpg';
 import q3 from '../My cat/q3.jpg';
 import q4 from '../My cat/q4.jpg';
+import q5 from '../src/My cat/q4.jpg';
+import q6 from '../src/My cat/q4.jpg';
+import q7 from '../src/My cat/q4.jpg';
+import q8 from '../src/My cat/q4.jpg';
+import q9 from '../src/My cat/q4.jpg';
+import q10 from '../src/My cat/q4.jpg';
+import q11 from '../src/My cat/q4.jpg';
+import q12 from '../src/My cat/q4.jpg';
+import q13 from '../src/My cat/q4.jpg';
+import q14 from '../src/My cat/q4.jpg';
+import q15 from '../src/My cat/q4.jpg';
+import q16 from '../src/My cat/q4.jpg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useState } from 'react';
 import { Button } from 'react-bootstrap';
@@ -11,7 +23,7 @@ import { Col } from 'react-bootstrap';
 export function ClickPre(){
     const [index,setIndex] = useState(0);
   
-    const [pic] = useState([q1, q2, q3, q4, QingQing]);
+    const [pic] = useState([QingQing, q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14, q15, q16]);
 
     const onClickPrevious= () => {
         if (index - 1 === -1 ){
